2977f187831acedaf5d0bf4d380dc43f1017b1c9
Maja18 <majadragojlovic98@gmail.com>
2021-02-07 20:10:12 +0100

    adding repositories

---
+++ Team22/src/main/java/ISA/Team22/Controller/PersonController.java
@@ -0,0 +1,5 @@
+package ISA.Team22.Controller;
+
+public class PersonController {
+
+}
---
+++ Team22/src/main/java/ISA/Team22/Repository/PatientRepository.java
@@ -0,0 +1,10 @@
+package ISA.Team22.Repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<PatientRepository, Long>{
+
+
+}
