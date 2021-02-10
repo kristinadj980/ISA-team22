93b1ac98b71f7554d8fb3bc9ddd5a8c342348163
Kristina Djuric <kristinadjuriic@gmail.com>
2021-02-10 18:06:30 +0100

    basic homepage look and bootstrap installation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <dermatologist-homepage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import DermatologistHomepage from './view/dermatologistView/DermatologistHomepage'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DermatologistHomepage
   }
 }
 </script>
